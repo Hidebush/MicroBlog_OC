1986811fb6324d7a4a4781e0cd63003e0159bb80
Theshy <475993847@qq.com>
2015-09-23 19:24:33 +0800

    初始化项目

---
+++ MicroBlog/MicroBlog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MicroBlog
+//
+//  Created by Theshy on 15/9/23.
+//  Copyright © 2015年 Theshy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MicroBlog/MicroBlog/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MicroBlog
+//
+//  Created by Theshy on 15/9/23.
+//  Copyright © 2015年 Theshy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
